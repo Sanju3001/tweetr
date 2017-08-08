eb532708f4dd0784c7ad5b6af11644ae37289249
Saj <codersaj@gmail.com>
2017-08-08 15:16:00 +0000

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,24 @@
+
+
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+
+/*
+app.get("/tweets", (req, res) => {
+
+  res.ridirect("/tweets");
+
+});
+
+
+
+app.post("/tweets", (req, res) => {
+
+  res.send(req.body.tweet);
+
+
+});
+*/
