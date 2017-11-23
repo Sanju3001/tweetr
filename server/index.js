aba5fd926fdb142d2ce775deff9672aa8e25a886
Saj <codersaj@gmail.com>
2017-11-22 23:03:35 -0500

    fix mongo conection string issue

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 "use strict";
+require('dotenv').config();
 
 // Basic express setup:
 
@@ -12,7 +13,7 @@ app.use(express.static("public"));
 
 // connect to Mongo DB
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
 
